df95659877d02137d60f065b36a924946acc1982
totallyharmlessgithub <joemama69@mailfence.com>
2024-05-30 08:24:34 +0000

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import pepe from "../Images/pepe.png";
+import { BrowserView, MobileView } from 'react-device-detect';
 export const About = (props) => {
   return (
-    <><div className="flex justify-center underline text-red-100 font-Carnivalee text-4xl drop-shadow-[3px_3px_var(--tw-shadow-color)] shadow-red-500 py-2 pb-10 opacity-80">
+    <><BrowserView>
+      <div className="flex justify-center underline text-red-100 font-Carnivalee text-4xl drop-shadow-[3px_3px_var(--tw-shadow-color)] shadow-red-500 py-2 pb-10 opacity-80">
     About This Clown
     <img src={pepe} height={70} width={70} alt="pepe" />
   </div>
-  <div className='mx-auto w-6/12'>{props.data}</div></>
+  <div className='mx-auto w-6/12'>{props.data}</div>
+    </BrowserView>
+    <MobileView>
+    <div className="flex justify-center underline text-red-100 font-Carnivalee text-4xl drop-shadow-[3px_3px_var(--tw-shadow-color)] shadow-red-500 py-2 pb-10 opacity-80">
+    About This Clown
+    <img src={pepe} height={70} width={70} alt="pepe" />
+  </div>
+  <div className='mx-auto w-full'>{props.data}</div>
+    </MobileView>
+    </>
   )
 }
