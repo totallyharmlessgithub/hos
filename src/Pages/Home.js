4d271b1690cb1ab992682e8b61ea3398cacea3f3
totallyharmlessgithub <joemama69@mailfence.com>
2024-05-30 21:18:25 +0000

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,32 +19,32 @@ import {
 
 export const Home = () => {
   const navigate = useNavigate();
-  const [name,setName] = useState("")
-  const [email,setEmail] = useState("")
-  const [message,setMessage] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
   const { isOpen, onOpen, onClose } = useDisclosure();
   const handleClick = () => {
     navigate("/search");
   };
-  const [trending,setTrending] = useState([]);
-  useEffect(()=>{
-    async function fetchTrending() { 
+  const [trending, setTrending] = useState([]);
+  useEffect(() => {
+    async function fetchTrending() {
       const supabase = createClient(process.env.REACT_APP_SUPAURL, process.env.REACT_APP_SUPAKEY);
       const { data, error } = await supabase
-      .from('clowns')
-      .select()
-      .order('rating',{ascending:false}).limit(3)
+        .from('clowns')
+        .select()
+        .order('rating', { ascending: false }).limit(3)
       setTrending(data)
     }
     fetchTrending()
-  },[])
+  }, [])
 
-  const submitContact = async () =>{
+  const submitContact = async () => {
     onClose()
     const supabase = createClient(process.env.REACT_APP_SUPAURL, process.env.REACT_APP_SUPAKEY);
     const { error } = await supabase
-    .from('contacts')
-    .insert({name:name,email:email,msg:message})
+      .from('contacts')
+      .insert({ name: name, email: email, msg: message })
   }
   return (
     <div className="justify-center text-center">
@@ -77,7 +77,7 @@ export const Home = () => {
           Trending Clowns <img src={pepe} alt="pepe" />
         </div>
         <div className="flex flex-row pb-20 gap-24 justify-center">
-          {trending!==null ? trending.map((clown)=><div onClick={()=>navigate("/profile?id=" + clown.profile_id)} className="flex flex-col">
+          {trending !== null ? trending.map((clown) => <div onClick={() => navigate("/profile?id=" + clown.profile_id)} className="flex flex-col">
             <div className="relative">
               <img
                 src={border}
@@ -103,8 +103,8 @@ export const Home = () => {
                 <img src={pepe} alt="pepe" />
               </div>
             </div>
-          </div>):<></>}
-          
+          </div>) : <></>}
+
         </div>
         <div className="flex flex-row justify-center  font-Carnivalee pt-24">
           <p className="text-4xl text-red-100 font-Carnivalee">
@@ -114,77 +114,77 @@ export const Home = () => {
             CREATE NOW
           </button>
           <ChakraProvider>
-              <Modal
-                className="bg-opacity-50"
-                isOpen={isOpen}
-                onClose={onClose}
-                isCentered
+            <Modal
+              className="bg-opacity-50"
+              isOpen={isOpen}
+              onClose={onClose}
+              isCentered
+            >
+              <ModalOverlay />
+              <ModalContent
+                bg="transparent"
+                p={4}
+                borderRadius="sm"
+                maxW="700px"
               >
-                <ModalOverlay />
-                <ModalContent
-                  bg="transparent"
-                  p={4}
-                  borderRadius="sm"
-                  maxW="700px"
-                >
-                  <Box className="p-5 rounded-lg bg-opacity-80 bg-red-500 text-2xl">
-                  
-                      <label
-                        htmlFor="email"
-                        className="block mb-2  font-medium"
-                      >
-                        Email:
-                      </label>
-                      <input
-                        required
-                        placeholder="Enter your mail to get notified about report updates"
-                        type="email"
-                        id="email"
-                        name="email"
-                        onChange={(event)=>setEmail(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5"
-                      />
-                      <br />
-                      <label
-                        htmlFor="name"
-                        className="block mb-2  font-medium "
-                      >
-                        Name:
-                      </label>
-                      <input
-                        required
-                        placeholder="Enter your name for report verification"
-                        type="text"
-                        id="name"
-                        name="name"
-                        onChange={(event)=>setName(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
-                      />
-                      <br />
-                      <label
-                        htmlFor="description"
-                        className="block mb-2 font-medium"
-                      >
-                        Message:
-                      </label>
-                      <textarea
-                        required
-                        placeholder="Write your Message here"
-                        id="message"
-                        name="message"
-                        onChange={(event)=>setMessage(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
-                      ></textarea>
-                      <br />
-                      <button
-                        onClick={()=>submitContact}
-                        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
-                      >Submit</button>
-                   
-                  </Box>
-                </ModalContent>
-              </Modal>
-            </ChakraProvider>
+                <Box className="p-5 rounded-lg bg-opacity-80 bg-red-500 text-2xl">
+
+                  <label
+                    htmlFor="email"
+                    className="block mb-2  font-medium"
+                  >
+                    Email:
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter your mail to get notified about report updates"
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5"
+                  />
+                  <br />
+                  <label
+                    htmlFor="name"
+                    className="block mb-2  font-medium "
+                  >
+                    Name:
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter your name for report verification"
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={(event) => setName(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
+                  />
+                  <br />
+                  <label
+                    htmlFor="description"
+                    className="block mb-2 font-medium"
+                  >
+                    Message:
+                  </label>
+                  <textarea
+                    required
+                    placeholder="Write your Message here"
+                    id="message"
+                    name="message"
+                    onChange={(event) => setMessage(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
+                  ></textarea>
+                  <br />
+                  <button
+                    onClick={() => submitContact}
+                    className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
+                  >Submit</button>
+
+                </Box>
+              </ModalContent>
+            </Modal>
+          </ChakraProvider>
         </div>
       </BrowserView>
       <MobileView>
@@ -216,21 +216,21 @@ export const Home = () => {
           <p>Trending Clowns</p>{" "}
           <img className="w-36 h-24" src={pepe} alt="pepe" />
         </div>
-        <div className="flex flex-col pb-20 gap-14 justify-center">
-          {trending!==null?trending.map((clown)=><div onClick={()=>navigate("/profile?id=" + clown.profile_id)} className="flex flex-col items-center w-full mx-auto">
-            <div className="relative">
+        <div className="flex flex-col pb-20 gap-14 justify-center lg:scale-75">
+          {trending !== null ? trending.map((clown) => <div onClick={() => navigate("/profile?id=" + clown.profile_id)} className="flex flex-col items-center w-full mx-auto">
+            <div className="relative px-5">
               <img
                 src={border}
                 alt="border"
-                height={500}
-                width={400}
+                height={600}
+                width={300}
                 className="relative rounded-2xl"
               />
               <img
                 src={clown.clown_pic}
-                height={460}
-                width={360}
-                className="absolute rounded-lg top-4 left-5"
+                height={580}
+                width={270}
+                className="absolute rounded-lg top-3 left-9 object-cover h-80"
                 alt="pic"
               />
             </div>
@@ -243,7 +243,7 @@ export const Home = () => {
                 <img src={pepe} alt="pepe" />
               </div>
             </div>
-          </div>):<></>}
+          </div>) : <></>}
         </div>
         <div className="flex flex-row justify-center  font-Carnivalee pt-24">
           <p className="text-4xl text-red-100 font-Carnivalee">
@@ -253,77 +253,77 @@ export const Home = () => {
             CREATE NOW
           </button>
           <ChakraProvider>
-              <Modal
-                className="bg-opacity-50"
-                isOpen={isOpen}
-                onClose={onClose}
-                isCentered
+            <Modal
+              className="bg-opacity-50"
+              isOpen={isOpen}
+              onClose={onClose}
+              isCentered
+            >
+              <ModalOverlay />
+              <ModalContent
+                bg="transparent"
+                p={4}
+                borderRadius="sm"
+                maxW="700px"
               >
-                <ModalOverlay />
-                <ModalContent
-                  bg="transparent"
-                  p={4}
-                  borderRadius="sm"
-                  maxW="700px"
-                >
-                  <Box className="p-5 rounded-lg bg-opacity-80 bg-red-500 text-2xl">
-                  
-                      <label
-                        htmlFor="email"
-                        className="block mb-2  font-medium"
-                      >
-                        Email:
-                      </label>
-                      <input
-                        required
-                        placeholder="Enter your mail to get notified about report updates"
-                        type="email"
-                        id="email"
-                        name="email"
-                        onChange={(event)=>setEmail(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5"
-                      />
-                      <br />
-                      <label
-                        htmlFor="name"
-                        className="block mb-2  font-medium "
-                      >
-                        Name:
-                      </label>
-                      <input
-                        required
-                        placeholder="Enter your name for report verification"
-                        type="text"
-                        id="name"
-                        name="name"
-                        onChange={(event)=>setName(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
-                      />
-                      <br />
-                      <label
-                        htmlFor="description"
-                        className="block mb-2 font-medium"
-                      >
-                        Message:
-                      </label>
-                      <textarea
-                        required
-                        placeholder="Write your Message here"
-                        id="message"
-                        name="message"
-                        onChange={(event)=>setMessage(event.target.value)}
-                        className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
-                      ></textarea>
-                      <br />
-                      <button
-                        onClick={()=>submitContact}
-                        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
-                      >Submit</button>
-                   
-                  </Box>
-                </ModalContent>
-              </Modal>
-            </ChakraProvider>
+                <Box className="p-5 rounded-lg bg-opacity-80 bg-red-500 text-2xl">
+
+                  <label
+                    htmlFor="email"
+                    className="block mb-2  font-medium"
+                  >
+                    Email:
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter your mail to get notified about report updates"
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5"
+                  />
+                  <br />
+                  <label
+                    htmlFor="name"
+                    className="block mb-2  font-medium "
+                  >
+                    Name:
+                  </label>
+                  <input
+                    required
+                    placeholder="Enter your name for report verification"
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={(event) => setName(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
+                  />
+                  <br />
+                  <label
+                    htmlFor="description"
+                    className="block mb-2 font-medium"
+                  >
+                    Message:
+                  </label>
+                  <textarea
+                    required
+                    placeholder="Write your Message here"
+                    id="message"
+                    name="message"
+                    onChange={(event) => setMessage(event.target.value)}
+                    className="text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 "
+                  ></textarea>
+                  <br />
+                  <button
+                    onClick={() => submitContact}
+                    className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
+                  >Submit</button>
+
+                </Box>
+              </ModalContent>
+            </Modal>
+          </ChakraProvider>
         </div>
       </MobileView>
     </div>
